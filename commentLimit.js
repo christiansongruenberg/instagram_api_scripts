c5d5a7b3e4239ef7178193464648255af84b4b1e
Christian Gruenberg <christian@crowdbabble.com>
2016-08-28 16:42:31 -0400

    most updated instagram api

--- commentLimit.js
+++ commentLimit.js
@@ -1,13 +1,15 @@
 
 var ig = require('instagram-node').instagram();
 
+ig.use({access_token: '237161643.1677ed0.5d3ef87bec2a4f598f0d0034557cbf26'});
 
-ig.use({ client_id: '654ff94de9ee44cead7e4de4891d5d18',
-    client_secret: 'e96b9424eee14238a947738fc4e6a753' });
+// ig.use({ client_id: '654ff94de9ee44cead7e4de4891d5d18',
+//     client_secret: 'e96b9424eee14238a947738fc4e6a753' });
 
 var mediaRes = function(err, medias, pagination, remaining, limit) {
+    if (err) throw err;
     console.log(medias[0].id);
-    ig.comments(medias[25].id, function(err, result, remaining, limit) {
+    ig.comments(medias[0].id, function(err, result, remaining, limit) {
         console.log(result.length);
     });
 };
--- miran.js
+++ miran.js
@@ -6,15 +6,24 @@
  */
 var ig = require('instagram-node').instagram();
 var async = require('async');
-var jsonfile = require('jsonfile')
+var jsonfile = require('jsonfile');
 
-ig.use({ client_id: '654ff94de9ee44cead7e4de4891d5d18',
-    client_secret: 'e96b9424eee14238a947738fc4e6a753' });
+/*ig.use({ client_id: '654ff94de9ee44cead7e4de4891d5d18',
+    client_secret: 'e96b9424eee14238a947738fc4e6a753' });*/
+
+ig.use({ access_token: '20880329.1677ed0.1fd65ee2e9314a81a22fbfeaced66d03' });
+
+/*ig.use({ client_id: '597b43061ec24a068d9352a403dc0738',
+    client_secret: '19adac0bdfab4ceebc7cc9eec0985b0b' });*/
 
 var allMedia = [];
 
 var hd2 = function(err, medias, pagination, remaining, limit) {
 
+    if(err){
+        console.log(err);
+        throw err;
+    }
 
     allMedia = allMedia.concat(medias);
     if(pagination.next){
@@ -35,7 +44,7 @@ var test = function(err, medias, pagination, remaining, limit) {
     console.log()
 };
 
-ig.user_followers('1523697664',{count: 100}, hd2);
+ig.user_followers('20880329',{count: 100}, hd2);
 
 
 
